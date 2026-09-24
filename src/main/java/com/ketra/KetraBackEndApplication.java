ad3f1746d3504ff06334e7721fa3f264e20363b6
Emanuel Carmona Gaviria <emanuel.carmona0467@uco.net.co>
2026-09-24 13:40:32 -0500

    Cambiar a estructura de KETRA Backend

---
+++ src/main/java/com/ketra/KetraBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ketra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KetraBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KetraBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ketra/KetraBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ketra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KetraBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
